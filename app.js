da1510b05ac3976f715d112ebae063cedaf453cd
Katamaranas <ernestas.zidokas@gmail.com>
2019-05-06 19:19:15 +0300

    guest list with Array.from and forEach functions for learning

--- app.js
+++ app.js
@@ -16,31 +16,46 @@ document.querySelector('#buttons').addEventListener('click', (event)=>{
       let userInput = document.querySelector('#userInput').value;
       if(userInput !== ''){
         usersArray.unshift(userInput);
-        }
-      break;
       }
+      break;
+    }
     case 'removeGuestFirst':
-      usersArray.shift(userInput);
-      break;
-      case 'removeGuestLast':
-      usersArray.pop(userInput);
-      break;
+    usersArray.shift(userInput);
+    break;
+    case 'removeGuestLast':
+    usersArray.pop(userInput);
+    break;
     case 'reverse':
-      usersArray.reverse();
-      break;
+    usersArray.reverse();
+    break;
     case 'clearLocalStorage':
-      clearLocalStorage('guests');
-      clearList();
-      clearInput();
-      usersArray = [];
-      break;
+    clearLocalStorage('guests');
+    clearList();
+    clearInput();
+    usersArray = [];
+    break;
     case 'removeFromTo':
-      let spliceStart = document.querySelector('#spliceStart').value;
-      let spliceDelete = document.querySelector('#spliceDelete').value;
-      usersArray.splice(Number(spliceStart)-1, spliceDelete);
-      break;
+    let spliceStart = document.querySelector('#spliceStart').value;
+    let spliceDelete = document.querySelector('#spliceDelete').value;
+    usersArray.splice(Number(spliceStart)-1, spliceDelete);
+    break;
+    case 'sortAZ':
+    usersArray.sort((a, b)=> {
+      return a.localeCompare(b);
+    });
+    break;
+    case 'sortZA':
+    usersArray.sort((a, b)=> {
+      return b.localeCompare(a);
+    });
+    break;
+    case 'replace':
+    let lookingName = document.querySelector('#lookingName').value;
+    let replaceName = document.querySelector('#replaceName').value;
+    findAndReplaceUser(usersArray, lookingName, replaceName);
+    break;
     default:
-      break;
+    break;
   }
   clearList();
   document.querySelector('#guests').appendChild(createList(usersArray));
@@ -79,4 +94,35 @@ function createList(usersArray) {
   });
 
   return container;
+}
+
+function findAndReplaceUser(arr, str1, str2) {
+  arr.find((name, index)=>{
+    if(name === str1){
+      arr.splice(index, 1, str2);
+      findAndReplaceUser(arr, str1, str2);
+    }
+  })
+
+  return arr;
+}
+
+function findReplaceArrayFrom(arr, str1, str2) {
+  Array.from(arr,(name)=>{
+    if(name == str1) {
+      arr.splice(arr.indexOf(str1), 1, str2);
+    }
+  })
+
+  return arr;
+}
+
+function findReplaceForeach(arr, str1, str2) {
+  arr.forEach((name, index) => {
+    if(name == str1) {
+      arr.splice(index, 1, str2);
+    }
+  })
+
+return arr;
 }
